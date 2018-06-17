6a3b22523cdf5b6eaa947893f2f3e7034452ad9d
Shea Rogers <shearogers@thinkforge.io>
2018-06-16 20:54:02 -0400

    fix tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,12 +19,12 @@ class TestDevelopmentConfig(TestCase):
         Test that the development configs are set correctly.
         :return:
         """
-        self.assertFalse(app.config['SECRET_KEY'] is 'john kagga')
+        self.assertFalse(app.config['SECRET_KEY'] is 'wrong key')
         self.assertTrue(app.config['DEBUG'], True)
         self.assertTrue(app.config['BCRYPT_HASH_PREFIX'] == 4)
         self.assertFalse(current_app is None)
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL',
-                                                                           "postgresql://postgres:123456@localhost/api"))
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] ==
+                        os.getenv('DATABASE_URL', "postgresql://postgres:postgres@localhost/api"))
         self.assertEqual(app.config['AUTH_TOKEN_EXPIRY_DAYS'], 1)
         self.assertEqual(app.config['AUTH_TOKEN_EXPIRY_SECONDS'], 20)
         self.assertEqual(app.config['BUCKET_AND_ITEMS_PER_PAGE'], 4)
@@ -44,13 +44,13 @@ class TestTestingConfig(TestCase):
         Test that the testing configs are set correctly
         :return:
         """
-        self.assertFalse(app.config['SECRET_KEY'] is 'john kagga')
+        self.assertFalse(app.config['SECRET_KEY'] is 'wrong key')
         self.assertTrue(app.config['DEBUG'], True)
         self.assertTrue(app.config['TESTING'] is True)
         self.assertTrue(app.config['BCRYPT_HASH_PREFIX'] == 4)
         self.assertFalse(current_app is None)
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL_TEST',
-                                                                           "postgresql://postgres:123456@localhost/api_test"))
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI']
+                        == os.getenv('DATABASE_URL_TEST', "postgresql://postgres:postgres@localhost/api_test"))
         self.assertEqual(app.config['AUTH_TOKEN_EXPIRY_DAYS'], 0)
         self.assertEqual(app.config['AUTH_TOKEN_EXPIRY_SECONDS'], 3)
         self.assertEqual(app.config['AUTH_TOKEN_EXPIRATION_TIME_DURING_TESTS'], 5)
